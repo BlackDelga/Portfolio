ace374d2f001fa041a656c1aa20b8ba7d1cfaf6c
Evandro Delgado <evanbdelgado@gmail.com>
2019-10-23 19:02:23 -0400

    Fixing the display and router of my project

--- app.js
+++ app.js
@@ -9,15 +9,15 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const { projects }  = require('./data.json');
-console.log(projects);
+const data = require('./data.json');
+const projects = data.projects;
 
 app.set('view engine', 'pug');
 
 app.use('/cssFiles', express.static('Public'));
 
 app.get('/', function (req, resp) {
-  resp.render('index', { projects });
+  resp.render('index', { projects, });
 });
 
 app.get('/contact.html', function (req, resp) {
@@ -29,11 +29,46 @@ app.get('/about.html', function (req, resp) {
 });
 
 app.get('/index.html', function (req, resp) {
-  resp.render('index', { projects });
+  resp.render('index', { projects, });
 });
 
-app.get('/project.html', function (req, resp) {
-  resp.render('project', { projects });
+app.get('/project', function (req, resp) {
+  resp.redirect('project/0');
+});
+
+//renders each project page based on pug template
+app.get('/project/:id', (req, res, next) => {
+  const {
+    id,
+  } = req.params;
+  if ((id >= 0 && id <= 4) && isNaN(id) === false) {
+    const project = projects[id];
+    res.render('project', {
+      project,
+      id,
+    });
+  } else {
+    next();
+  }
+});
+
+// Creating new error
+app.use((req, res, next) => {
+  const err = new Error('There has been an error!');
+  err.status = 404;
+  next(err);
+});
+
+//using that error to display error pug template
+app.use((err, req, res, next) => {
+  res.status(err.status);
+  err.message = 'There has been an error';
+  res.locals.error = err;
+  if (res.status = 404) {
+    console.log(`${req.path} is not a valid path`);
+  }
+
+  res.render('error');
 });
 
 app.listen(3000, function () {
